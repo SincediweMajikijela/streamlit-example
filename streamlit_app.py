073b463251e5459a4403fcb076ef7b336aab2916
SincediweMajikijela <130060110+SincediweMajikijela@users.noreply.github.com>
2023-04-23 19:44:18 +0200

    Create streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,35 +4,20 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import numpy as np 
+import matplotlib.pyplot as plt
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+st.set_page_config
+{
+page_title = “Busiset Airport”.
+page_icon = “Active”,
+layout = “wide”,
+} 
+st.title(“Popular Destinations in South Africa”)
+st.markdown(“This dashboard will assist you get more information about Popular Destinations in South Africa”)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+st.sidebar.title(“Selecting Visual Charts”)
+st.sidebar.markdown(“Selecting the Charts accordingly.”) 
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
