3ff84162636cd8cbc035eedddabfed7cb86b525e
SincediweMajikijela <130060110+SincediweMajikijela@users.noreply.github.com>
2023-04-25 13:24:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,4 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-st.set_page_config
-{
-page_title = “Busiset Airport”.
-page_icon = “Active”,
-layout = “wide”,
-} 
-st.title(“Popular Destinations in South Africa”)
-st.markdown(“This dashboard will assist you get more information about Popular Destinations in South Africa”)
-
-st.sidebar.title(“Selecting Visual Charts”)
-st.sidebar.markdown(“Selecting the Charts accordingly.”) 
-
+st.write('sne")
