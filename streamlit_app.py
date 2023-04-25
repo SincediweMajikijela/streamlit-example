032d7f73a045fa57da38c77a841fa5bca680bca6
Michelle-Madiba <130059963+Michelle-Madiba@users.noreply.github.com>
2023-04-25 18:07:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,11 @@ import streamlit as st
 st.set_page_config(page_title="Flights Dashboard", page_icon=":bar_chart:", layout="wide")
 
 # ---- READ FILES ----
-dfAirport = pd.read_file('airports.dat')
-dfAirline = pd.read_file('airliness.dat')
-dfPlane=pd.read_file('planes.dat')
-dfCountry=pd.read_file('countries.dat')
-dfRoute=pd.read_file('routes.dat')
+dfAirport = pd.read_csv('airports.dat')
+dfAirline = pd.read_csv('airliness.dat')
+dfPlane=pd.read_csv('planes.dat')
+dfCountry=pd.read_csv('countries.dat')
+dfRoute=pd.read_csv('routes.dat')
 
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
